1e1afda0a5a925dba5d9fa349af437fc520f2c81
kimbomi <bomi258@naver.com>
2023-02-28 12:55:19 +0900

    카카오톡 비밀번호 확인 화면 만들기

--- proapplication3/src/main/java/com/example/proapplication3/MainActivity.kt
+++ proapplication3/src/main/java/com/example/proapplication3/MainActivity.kt
@@ -2,21 +2,20 @@ package com.example.proapplication3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.proapplication3.databinding.ActivityViewBinding
 
 class MainActivity : AppCompatActivity() {
-    private var mBinding: ActivityViewBinding? = null
-    private val binding get() = mBinding!!
+    /*private var mBinding: ActivityViewBinding? = null
+    private val binding get() = mBinding!!*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_viewbind)
-        mBinding = ActivityViewBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_view2)
+        /*mBinding = ActivityViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.tvMessage.setText("안녕하세요 김보미입니다.")
+        binding.tvMessage.setText("안녕하세요 김보미입니다.")*/
     }
 
-    override fun onDestroy() {
+ /*   override fun onDestroy() {
         mBinding = null
         super.onDestroy()
-    }
+    }*/
 }
